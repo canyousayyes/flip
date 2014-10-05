c37d1ce45a4266a3c92468c8328f7496e6567c7b
Fishball Ng <fishballium@gmail.com>
2014-10-05 21:30:40 +0800

    Not using freeze method as it is buggy

--- assets/scripts/modules/tiles/tile_view.js
+++ assets/scripts/modules/tiles/tile_view.js
@@ -5,44 +5,39 @@ define('tile_view', ['backbone', 'underscore', 'text!template_tile_view'], funct
         events: {
             'click': 'tap'
         },
-        is_freezed: false,
         initialize: function () {
             this.listenTo(this.model, 'change', this.renderFlip);
-            this.render();
+            this.render({tile_class: ''}); // Init tile view with not flipped state
         },
         // Compose an object from its model for rendering the view
         getRenderData: function () {
             var color = this.model.get('color');
             var obj = {
                 frontColorClass: (color === 'white' ? 'tile_white' : 'tile_black'),
-                backColorClass: (color === 'white' ? 'tile_black' : 'tile_white')
+                backColorClass: (color === 'white' ? 'tile_black' : 'tile_white'),
+                tile_class: 'tile_flipped'
             };
             return obj;
         },
         // Render the html based on its model
-        render: function () {
-            var default_option = { flipped: true };
+        render: function (option) {
+            if ( typeof option !== 'object' ) {
+                option = {};
+            }
+            var data = _.extend(this.getRenderData(), option);
             var template = _.template(template_tile_view);
-            var rendered_html = template(this.getRenderData());
+            var rendered_html = template(data);
             this.$el.html(rendered_html);
             return this;
         },
         // Render the view and also add a flipping animation
         renderFlip: function () {
             var self = this;
-            // Freeze the view until the animation is completed.
-            this.is_freezed = true;
             this.render();
-            this.$('.tile').addClass('tile_flipped');
-            // Transition end event reference: http://stackoverflow.com/questions/2794148/css3-transition-events
-            this.$('.tile').on('webkitTransitionEnd oTransitionEnd transitionend', function () {
-                self.$('.tile').removeClass('tile_animated');
-                self.is_freezed = false;
-            });
-            // Remove the class to trigger the transfomation. the flipping animation is written in styles/site.less
+            // Remove class to trigger animation
             setTimeout(function () {
-                self.$('.tile').addClass('tile_animated').removeClass('tile_flipped');
-            }, 10);
+                self.$('.tile').removeClass('tile_flipped');
+            }, 100);
         },
         // Trigger the tap event to its model, and let its model to handle what to do
         tap: function () {
